b3ad5c7c1a7b8eb50f24dc8ff6bb377d73b7118b
Bill Centinaro <bill@aethertech.org>
2024-10-26 03:12:23 +0000

    Tailwind Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@ module.exports = {
     content: [
         './_layouts/*.html',
         './_includes/*.html',
+        './_includes/**/*.html',
         './_**/*.md',
         './*.md',
         './*.html',
+        './tina/config.ts'
     ],
     theme: {
 
@@ -19,37 +21,7 @@ module.exports = {
                         primary: '#0a0a0a',
                         secondary: '#0a0a0a',
                         background: '#f5f5f5',
-                    },
-                    ceil_nior: {
-                        primary: '#2e1065',
-                        secondary: '#f5f5f5',
-                        background: '#f5f5f5',
-                    },
-                    everbloom: { 
-                        primary: '#022c22', 
-                        secondary: '#022c22', 
-                        background: '#f5f5f5' 
-                    },
-                    taraj: { 
-                        primary: '#0c457d', 
-                        secondary: '#0c457d', 
-                        background: '#f5f5f5' 
-                    },
-                    nivalia: { 
-                        primary: '#0d56ba', 
-                        secondary: '#474444', 
-                        background: '#f5f5f5' 
-                    },
-                    hirutai: { 
-                        primary: '#8f2323', 
-                        secondary: '#8f2323', 
-                        background: '#f5f5f5' 
-                    },
-                    celestia: { 
-                        primary: '#04001e', 
-                        secondary: '#04001e', 
-                        background: '#f5f5f5' 
-                    },
+                    }
                 }
             },
         },
