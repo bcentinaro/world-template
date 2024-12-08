5740c374130050a2804408e4bc240866fb39e864
Bill Centinaro <bill@aethertech.org>
2024-12-08 16:30:46 +0000

    Changes

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 bundle install 
 npm install 
-npx tinacms dev -p 5321 -c "bundle exec jekyll serve --watch --livereload --drafts --host 0.0.0.0 -P 4321"
+npx tinacms dev -p 5321 -c "bundle exec jekyll serve --watch --incremental --drafts --host 0.0.0.0 -P 4321"
